a5dc2f80b9e308edb0f005b71045a8ed614ad85a
JESBA8F <jestravis@paradigmadigital.com>
2022-09-22 11:00:51 +0200

    Primer commit funcionando

---
+++ src/main/java/com/paradigmaopenspace/graphqlclient/GraphqlClientApplication.java
@@ -0,0 +1,73 @@
+package com.paradigmaopenspace.graphqlclient;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.graphql.client.HttpGraphQlClient;
+import reactor.core.publisher.Mono;
+
+import java.time.Duration;
+import java.time.temporal.ChronoUnit;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Stream;
+
+@SpringBootApplication
+public class GraphqlClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraphqlClientApplication.class, args);
+	}
+
+	// BBDD
+	List<Artista> otraBBDD=List.of(
+			new Artista("Levstein", 1991),
+			new Artista("Molina", 1988),
+			new Artista("Casile", 1992),
+			new Artista("Federovisky", 1994)
+	);
+
+	// Repository / Service
+	int buscarAnoDelArtista(String apellido){
+		return otraBBDD.parallelStream()
+				.filter(artista -> artista.apellido().equalsIgnoreCase(apellido))
+				.mapToInt(Artista::nacimiento)
+				.findFirst()
+				.orElseGet(()->0);
+	}
+
+	@Bean
+	HttpGraphQlClient graphQlClient(){
+		return HttpGraphQlClient.builder()
+				.url("http://localhost:8181/graphql")
+				.build();
+	}
+
+	@Bean
+	CommandLineRunner inicio(HttpGraphQlClient graphQlClient) {
+		return (args) -> {
+			System.out.println("Estos son los artistas de los cuales hemos encontrado su fecha de nacimiento:");
+			graphQlClient
+					.document("""
+								query{
+									 artistas{
+										 apellido
+									 }
+								 }
+							""")
+					.retrieve("artistas")
+					.toEntityList(Artista.class)
+					.map(artistas -> artistas.stream()
+							.map(artista -> new Artista(artista.apellido(), buscarAnoDelArtista(artista.apellido())))
+					)
+					.block(Duration.of(4, ChronoUnit.SECONDS))
+					.filter(artista -> artista.nacimiento() > 0)
+					.forEach(System.out::println);
+		};
+	}
+}
+
+//Mi definición de artista, distinta a la de la API externa
+record Artista(String apellido, Integer nacimiento) {
+}
---
+++ src/test/java/com/paradigmaopenspace/graphqlclient/GraphqlClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paradigmaopenspace.graphqlclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphqlClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
